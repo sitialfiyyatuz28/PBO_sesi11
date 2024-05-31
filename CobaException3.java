a49a7a32f3bf91553a72120ff316dd1330729856
Siti Alfiyyatuz Zakiyyah Alawiyyah <114629594+sitialfiyyatuz28@users.noreply.github.com>
2024-05-31 21:39:11 +0700

    Add files via upload

---
+++ CobaException3.java
@@ -0,0 +1,19 @@
+
+package Tugas10;
+
+
+public class CobaException3 {
+    
+    public static void main(String[] args) {
+        int bil=10;
+        try
+        {
+            System.out.println("bil/o");
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Terjadi Aritmatika error");
+        }
+    }
+    
+}
